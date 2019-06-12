e9abde0cd9028209055cf789979d0aebbaa9480c
Kaitlin Park <kaitlinpark@parkaitlin-mac.local>
2019-06-12 10:40:44 -0700

    add region and city component

--- src/components/TEST/test.jsx
+++ src/components/TEST/test.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { transportation } from '../../MODELS/TRANSPORTATION'
 
-const Test = () => {
-  return (
-    <div>
-      <h1>These are the regions.</h1>
+class Resources extends Component {
+  state = {
+
+  }
+  render(){
+    return (
+      <div>
+      <h1>Please select your Region</h1>
       <p>
         { transportation[0].PROGRAM_NAME }
 
       </p>
     </div>
-  )
+    )
+  }
 }
 
 export default Test
