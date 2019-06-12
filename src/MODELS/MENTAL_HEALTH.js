74d77c05a3fef971b902096e1ed12d5d82adaaa8
Alex Hughes <alexhughes@AlexMBPro.local>
2019-06-11 18:32:12 -0700

    Populated Mental_Health.js

--- src/MODELS/MENTAL_HEALTH.js
+++ src/MODELS/MENTAL_HEALTH.js
@@ -1,17 +1,157 @@
-// object model for each resource
-{
-    programName: "",
-    spa: "",
-    operator: "",
-    city: "",
-    address: "",
-    website: "",
-    phone: "",
-    hours: "",
-    walkIn: boolean,
-    eligibility: ""
-}
+// // object model for each resource
+// {
+//     programName: "",
+//     spa: "",
+//     operator: "",
+//     city: "",
+//     address: "",
+//     website: "",
+//     phone: "",
+//     hours: "",
+//     walkIn: boolean,
+//     eligibility: ""
+// }
 
 const mentalHealth = [
-
+[
+{
+    "Program Name": "Daniel's Place",
+    "Service Planning/Geographic Area": "SPA 5 - West LA",
+    "Operator": "Step Up on Second",
+    "Neighborhood": "Santa Monica",
+    "Address": "1619 Santa Monica Blvd, Santa Monica, CA 90404",
+    "Website": "http://danielsplace.org/",
+    "Phone": "310-392-5855",
+    "Hours of Operation": "Mon: 7:00am-4:00pm, Tue-Fri: 10am-7:00pm, Sat: 9:30am-5:30pm and Sun: 9:00am-1:00pm",
+    "Walk-Ins Allowed?": "Yes",
+    "Eligibility Requirements": "Ages 18-28 only"
+},
+{
+    "Program Name": "Compton Mental Health ",
+    "Service Planning/Geographic Area": "SPA 6 - Central LA",
+    "Operator": "DMH",
+    "Neighborhood": "Compton",
+    "Address": "921 East Compton Boulevard, Compton, CA 90221",
+    "Website": "dmh.lacounty.gov",
+    "Phone": "310-668-6800",
+    "Hours of Operation": "8:00am-5:00pm",
+    "Walk-Ins Allowed?": "Yes"
+},
+{
+    "Program Name": "Exodus/MLK Urgent Care Center",
+    "Service Planning/Geographic Area": "SPA 6 - Central LA",
+    "Operator": "Exodus",
+    "Neighborhood": "Hacienda Village",
+    "Address": "12021 S Wilmington Ave Los Angeles, Ca 90059",
+    "Phone": "562-295-4617",
+    "Hours of Operation": "24/7"
+},
+{
+    "Program Name": "San Pedro Mental Health Clinic",
+    "Service Planning/Geographic Area": "SPA 8 - South Bay",
+    "Operator": "LA County Department of Mental Health",
+    "Neighborhood": "San Pedro",
+    "Address": "150 WEST 7TH ST. SAN PEDRO CA 90731 ",
+    "Phone": "310-519-6100",
+    "Hours of Operation": "MON - FRI 8:00 a.m. - 5:30 p.m. Walk in Hours: MON-FRI 8:00 am- 4:30 pm",
+    "Walk-Ins Allowed?": "Yes"
+},
+{
+    "Program Name": "Exodus Recovery, Inc.",
+    "Service Planning/Geographic Area": "SPA 8 - South Bay",
+    "Operator": "Exodus Recovery, Inc.",
+    "Neighborhood": "Redondo Beach",
+    "Address": "923 South Catalina Avenue Redondo Beach, CA, 90277",
+    "Phone": "800-286-1182",
+    "Hours of Operation": "Monday through Friday 9:00 a.m. to 5:00 p.m",
+    "Eligibility Requirements": "ages of 26-59 only"
+},
+{
+    "Program Name": "Behavioral Health",
+    "Service Planning/Geographic Area": "SPA 7 - South East LA",
+    "Operator": "Via Care Community Health Center",
+    "Neighborhood": "East Los Angeles",
+    "Address": " 607 S Atlantic Blvd., Los Angeles, CA 90022",
+    "Website": "https://www.viacarela.org/",
+    "Phone": "(323) 268-9191"
+},
+{
+    "Program Name": "Mental Health America",
+    "Service Planning/Geographic Area": "SPA 1 - Antelope Valley",
+    "Operator": "Mental Health America",
+    "Neighborhood": "Lancaster",
+    "Address": "506 W Jackman St, Lancaster, CA 93534",
+    "Phone": "(661) 726-2850",
+    "Hours of Operation": "9:00 am - 5:00 PM Monday through Friday",
+    "Walk-Ins Allowed?": "No",
+    "Eligibility Requirements": "TAY, Family, Adult"
+},
+{
+    "Program Name": "LA County Dept. of Mental Health",
+    "Service Planning/Geographic Area": "SPA 1 - Antelope Valley",
+    "Operator": "LACDMH",
+    "Neighborhood": "Palmdale",
+    "Address": "1529 E. Palmdale Blvd., Ste. 150 Palmdale, CA 93550",
+    "Website": "dmh.lacounty.gov",
+    "Phone": "(800) 854-7771",
+    "Hours of Operation": "Monday through Sunday, 24 hours per day",
+    "Walk-Ins Allowed?": "No",
+    "Eligibility Requirements": "TAY, Family, Adult"
+},
+{
+    "Program Name": "Tarzana Treatment Center",
+    "Service Planning/Geographic Area": "SPA 1 - Antelope Valley",
+    "Operator": "Tarzana Treatment Center",
+    "Neighborhood": "Palmdale",
+    "Address": "320 E Palmdale Blvd, Palmdale, CA 93550",
+    "Phone": "(661) 729-9000",
+    "Hours of Operation": "Monday - Friday 8:00 AM - 4:30 PM",
+    "Walk-Ins Allowed?": "No",
+    "Eligibility Requirements": "TAY, Family, Adult"
+},
+{
+    "Program Name": "Shields for Families",
+    "Service Planning/Geographic Area": "SPA 6 - Central LA",
+    "Operator": "Shields for Families ",
+    "Neighborhood": "Compton",
+    "Address": "3221 Alameda St, Compton, CA 90222",
+    "Website": "Shieldsforfamilies.org",
+    "Phone": "323-242-5000",
+    "Hours of Operation": "Monday - Friday 8:00 AM - 4:30 PM",
+    "Walk-Ins Allowed?": "Yes",
+    "Eligibility Requirements": "TAY, Family, Adult"
+},
+{
+    "Program Name": "Didi Hirsh Metro Center",
+    "Service Planning/Geographic Area": "SPA 4 - Metro LA",
+    "Neighborhood": "Rampart Village",
+    "Address": "672 S La Fayette Park Pl Ste 6, Los Angeles",
+    "Phone": " (213) 381-3626",
+    "Hours of Operation": "M-F 8am-5pm",
+    "Walk-Ins Allowed?": "Yes",
+    "Eligibility Requirements": "TAY, Family, Adult"
+},
+{
+    "Program Name": "Downtown Mental Health Center",
+    "Service Planning/Geographic Area": "SPA 4 - Metro LA",
+    "Operator": "DMH",
+    "Neighborhood": "Downtown Los Angeles",
+    "Address": "529 Maple Ave Los Angeles",
+    "Phone": "213-430-6700",
+    "Hours of Operation": "M-F 8am-5pm",
+    "Walk-Ins Allowed?": "Yes",
+    "Eligibility Requirements": "TAY, Family, Adult"
+},
+{
+    "Program Name": "Stars Behavioral Health Group",
+    "Service Planning/Geographic Area": "SPA 4 - Metro LA",
+    "Operator": "DMH",
+    "Neighborhood": "Rampart Village",
+    "Address": "1625 W Olympic Blvd Los Angeles",
+    "Phone": "213-201-2950",
+    "Hours of Operation": "M-F 8am-5pm",
+    "Walk-Ins Allowed?": "Yes",
+    "Eligibility Requirements": "TAY, Family, Adult"
+}
+]
 ]
