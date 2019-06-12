c8f67e06dec175684c40ea12e68a2d3cf7e7632e
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-06-11 19:43:05 -0700

    Add json

--- src/MODELS/MENTAL_HEALTH.js
+++ src/MODELS/MENTAL_HEALTH.js
@@ -8,10 +8,178 @@
     website: "",
     phone: "",
     hours: "",
-    walkIn: boolean,
+    walkIn: "",
     eligibility: ""
 }
 
 const mentalHealth = [
+  {
+    programName: "Daniel's Place",
+    spa: "SPA 5 - West LA",
+    operator: "Step Up on Second",
+    city: "Santa Monica",
+    address: "1619 Santa Monica Blvd, Santa Monica, CA 90404",
+    website: "http://danielsplace.org/",
+    phone: "(310) 392-5855",
+    hours: "Monday: 7:00am-4:00pm, Tuesday-Friday: 10am-7:00pm, Saturday: 9:30am-5:30pm and Sunday: 9:00am-1:00pm",
+    walkIn: "Yes",
+    eligibility: "Ages 18-28 only"
+  },
+  {
+    programName: "Compton Mental Health",
+    spa: "SPA 6 - Central LA",
+    operator: "DMH",
+    city: "Compton",
+    address: "921 East Compton Boulevard, Compton, CA 90221",
+    website: "dmh.lacounty.gov",
+    phone: "(310) 668-6800",
+    hours: "8:00am-5:00pm",
+    walkIn: "Yes",
+    eligibility: "N/A"
+  },
+  {
+    programName: "Exodus/MLK Urgent Care Center",
+    spa: "SPA 6 - Central LA",
+    operator: "Exodus",
+    city: "Hacienda Village",
+    address: "12021 S Wilmington Ave Los Angeles, Ca 90059",
+    website: "N/A",
+    phone: "(562) 295-4617",
+    hours: "24/7",
+    walkIn: "N/A",
+    eligibility: "N/A"
+  },
+  {
+    programName: "San Pedro Mental Health Clinic",
+    spa: "SPA 8 - South Bay",
+    operator: "LA County Department of Mental Health",
+    city: "San Pedro",
+    address: "150 West 7th St. San Pedro CA 90731",
+    website: "N/A",
+    phone: "(310) 519-6100",
+    hours: "Monday - Friday 8:00am-5:30pm Walk in Hours: Monday-Friday 8:00am- 4:30pm",
+    walkIn: "Yes",
+    eligibility: "N/A"
+  },
+  {
+    programName: "Exodus Recovery, Inc.",
+    spa: "SPA 8 - South Bay",
+    operator: "Exodus Recovery, Inc.",
+    city: "Redondo Beach",
+    address: "923 South Catalina Avenue Redondo Beach, CA, 90277",
+    website: "N/A",
+    phone: "(800) 286-1182",
+    hours: "Monday through Friday 9:00am to 5:00pm",
+    walkIn: "N/A",
+    eligibility: "ages of 26-59 only"
+  },
+  {
+    programName: "Behavioral Health",
+    spa: "SPA 7 - South East LA",
+    operator: "Via Care Community Health Center",
+    city: "East Los Angeles",
+    address: "607 S Atlantic Blvd., Los Angeles, CA 90022",
+    website: "https://www.viacarela.org/",
+    phone: "(323) 268-9191",
+    hours: "N/A",
+    walkIn: "N/A",
+    eligibility: "N/A"
+  },
+  {
+    programName: "Mental Health America",
+    spa: "SPA 1 - Antelope Valley",
+    operator: "Mental Health America",
+    city: "Lancaster",
+    address: "506 W Jackman St, Lancaster, CA 93534",
+    website: "N/A",
+    phone: "(661) 726-2850",
+    hours: "9:00am-5:00pm Monday - Friday",
+    walkIn: "No",
+    eligibility: "TAY, Family, Adult"
+  },
+  {
+    programName: "LA County Dept. of Mental Health",
+    spa: "SPA 1 - Antelope Valley",
+    operator: "LACDMH",
+    city: "Palmdale",
+    address: "1529 E. Palmdale Blvd., Ste. 150 Palmdale, CA 93550",
+    website: "dmh.lacounty.gov",
+    phone: "(800) 854-7771",
+    hours: "Monday through Sunday, 24 hours per day",
+    walkIn: "No",
+    eligibility: "TAY, Family, Adult"
+  },
+  {
+    programName: "Tarzana Treatment Center",
+    spa: "SPA 1 - Antelope Valley",
+    operator: "Tarzana Treatment Center",
+    city: "",
+    address: "",
+    website: "",
+    phone: "",
+    hours: "",
+    walkIn: "",
+    eligibility: ""
+  },
+  {
+    programName: "",
+    spa: "",
+    operator: "",
+    city: "",
+    address: "",
+    website: "",
+    phone: "",
+    hours: "",
+    walkIn: "",
+    eligibility: ""
+  },
+  {
+    programName: "",
+    spa: "",
+    operator: "",
+    city: "",
+    address: "",
+    website: "",
+    phone: "",
+    hours: "",
+    walkIn: "",
+    eligibility: ""
+  },
+  {
+    programName: "",
+    spa: "",
+    operator: "",
+    city: "",
+    address: "",
+    website: "",
+    phone: "",
+    hours: "",
+    walkIn: "",
+    eligibility: ""
+  },{
+    programName: "",
+    spa: "",
+    operator: "",
+    city: "",
+    address: "",
+    website: "",
+    phone: "",
+    hours: "",
+    walkIn: "",
+    eligibility: ""
+  },
+  {
+    programName: "",
+    spa: "",
+    operator: "",
+    city: "",
+    address: "",
+    website: "",
+    phone: "",
+    hours: "",
+    walkIn: "",
+    eligibility: ""
+  }
 
+  
 ]
