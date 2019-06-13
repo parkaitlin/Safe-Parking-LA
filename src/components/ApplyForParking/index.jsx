fd8a78880fa76783cc058e7f03b24149abb3c8bd
vianneychin <47989727+vianneychin@users.noreply.github.com>
2019-06-12 17:23:06 -0700

    Finish

--- src/components/ApplyForParking/index.jsx
+++ src/components/ApplyForParking/index.jsx
@@ -7,25 +7,26 @@ const ApplyForParking = () => {
       <div>
 
         <h1>
-          Apply to park your car in
-          a designated parking lot.
-            </h1>
-        <h1>Amenties</h1>
+          Overnight Parking FAQ
+        </h1>
+        <h1>Amenities</h1>
+        <br />
         <ul>
           <li>A safe place to park each night</li>
           <li>Restroom access</li>
           <li>A security guard.</li>
           <li>Social Service resources.</li>
         </ul>
-        <h1>Process</h1>
+        <h1>Our Goal</h1>
+        <br />
         <ol>
-          <li> First we must decriminalize sleeping in a vehicle which in most of Los Angeles illegal.
+          <li> <span>First</span> we must decriminalize sleeping in a vehicle which in most of Los Angeles illegal.
           The City of LA is currently creating a Safe Parking Program that will provide case management and reach out to those in need of a safe parking spot.</li>
-          <li> Second we us our private donations to secure parking lots around the city as gathering points.</li>
-          <li>Lastly, we will brand these available parking lots as designated Safe Parking LA.
+          <li> <span>Second</span> we use our private donations to secure parking lots around the city as gathering points.</li>
+          <li><span>Lastly</span>, we will brand these available parking lots as designated Safe Parking LA.
           We reach out to those in need of a parking space and update the availability of a parking space.
           We will work with law enforcement for a 'grace period' with a vehicle with the Safe Parking LA map on the vehicle.
-          We will ultimately create gathering points around the city for parking lots that are unusued during evening hours as a safe place to park.</li>
+          We will ultimately create gathering points around the city for parking lots that are unusued during evening hours as a safe place to park for the night.</li>
         </ol>
         <section>
           <div>
@@ -33,6 +34,7 @@ const ApplyForParking = () => {
           </div>
           <div>
             <button>Interest Form</button>
+            <br />
             <button>Compliance Form</button>
           </div>
         </section>
--- src/components/ApplyForParking/styled.jsx
+++ src/components/ApplyForParking/styled.jsx
@@ -2,33 +2,74 @@ import styled from 'styled-components'
 
 export const MainContainer = styled.div`
   
-  background-color: grey;
-  height: 100vh;
+  height: 110vh;
   display: flex;
   justify-content: center;
+
   > div {
-    background-color: yellow;
-    width: 1000px;
-    ul {
-      li {
-        margin-bottom: .5em;
+      width: 1000px;
+      padding-right: 2em;
+      padding-left: 2em;
+      > h1:nth-of-type(1) {
+        text-align: center;
+        font-size: 3em;
+        padding-top: 1em;
       }
-    }
+      > h1:nth-of-type(2) {
+        padding-top: 3em;
+        text-align: center;
+      }
+      > h1:nth-of-type(3) {
+        padding-top: 3em;
+      }
+      ul {
+        li {
+          margin-bottom: 0.5em;
+          text-align: center;
+          list-style: none;
+          font-size: 1.5em;
+        }
+      }
     ol {
       li {
         margin-bottom: 1em;
+        padding-left: 1.5em;
+        padding-right: 1.5em;
+        list-style: none;
+        font-size: 1.5em;
+        span {
+          font-weight: bolder;
+          border-bottom: 1px solid black;
+        }
       }
     }
     section {
       display: flex;
+      margin-left: 1em;
+      margin-right: 1em;
+      margin-top: 3em;
+      div:nth-of-type(1) {
+        img {
+          height: 340.5px;
+          width: 512px;
+          border-radius: 10px;
+          padding-left: 1.5em;
+        }
+      }
       div:nth-of-type(2) {
         display: flex;
         flex-direction: column;
-      }
-      img {
-        height: 340.5px;
-        width: 512px;
-        border-radius: 10px;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        padding-left: 2em;
+        button {
+          width: 250px;
+          height: 50px;
+          border-radius: 4px;
+          border: 2px solid grey;
+          font-size: 1.2em;
+        }
       }
     }
   }
@@ -36,18 +77,32 @@ export const MainContainer = styled.div`
    display: block;
    > div {
       width: 100%;
-      background-color: blue;
+      > h1:nth-of-type(1) {
+          font-size: 1.em;
+      }
+      ol {
+        li {
+          font-size: 1.2em
+        }
+      }
       section {
         flex-direction: column;
-        img {
-          height: 170.25px;
-          width: 256px;
-        }
         div:nth-of-type(1) {
-          
+          display: flex;
+          justify-content: center;
+          img {
+            height: 170.25px;
+            width: 256px;
+            padding: 0;
+            padding-bottom: 1em;
+          }
         }
         div:nth-of-type(2) {
-
+          padding: 0;
+          padding-top: 2em;
+          padding-bottom: 2em;
+          button {
+          }
         }
       }
    }
